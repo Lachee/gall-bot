0cd87bf7fd603ef9f2bfc43541a9d6744449a1cb
Lachee <lachlan.h@hotmail.com>
2021-02-08 00:24:11 +1100

    Guild Configuration

--- index.mjs
+++ index.mjs
@@ -2,11 +2,27 @@ import dotenv from 'dotenv'; dotenv.config();   // Load Enviroment
 import { BaseAPI } from './Gall/BaseAPI.mjs';
 import Discord, { ReactionUserManager } from 'discord.js';               // The discord client
 import { Gallery } from './Gall/Types.mjs';
+import  Enmap from 'enmap';
 
 const gall          = new BaseAPI(`${process.env.GALL_URL}api`, process.env.GALL_TOKEN);
 const discord       = new Discord.Client();
 const galleryCache  = new Map();
 const userLock = {};
+
+discord.settings = new Enmap({
+    name: "settings",
+    fetchAll: false,
+    autoFetch: true,
+    cloneLevel: 'deep'
+});
+
+const defaultSettings = {
+    prefix: "$",
+    postGallery: true,
+    embedGallery: true,
+    supressEmbed: true,
+    channel: ''
+}
 
 /** When the bot is first ready, lets try and publish all the guilds we are in */
 discord.on('ready', async () => {
@@ -20,7 +36,65 @@ discord.on('ready', async () => {
 
 /** When we have a message, look for links on the message */
 discord.on('message', async (msg) => {
-    if (msg.author.bot) return;
+    if(!message.guild || message.author.bot) return;
+
+    
+    //Process a command
+    const conf = discord.settings.ensure(member.guild.id, defaultSettings);
+    if (msg.content.indexOf(config.prefix) === 0) {
+        await processMessageCommand(conf.prefix, msg);
+        return;
+    }
+
+    //Process image uploads
+    if (msg.channel.id == conf.channel) {
+        await processMessageUpload(msg);
+        return;
+    }
+});
+
+/** Processes a message and try to execute commands found in it. */
+async function processMessageCommand(prefix, message) {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    switch(command) {
+        default: break;
+        case 'setconf': 
+            // Command is admin only, let's grab the admin value: 
+            const adminRole = message.guild.roles.cache.find("name", guildConf.adminRole);
+            if(!adminRole) return message.reply("Administrator Role Not Found");
+
+            // Then we'll exit if the user is not admin
+            if(!message.member.roles.cache.has(adminRole.id)) {
+            return message.reply("You're not an admin, sorry!");
+            }
+
+            // Let's get our key and value from the arguments. 
+            // This is array destructuring, by the way. 
+            const [prop, ...value] = args;
+            // Example: 
+            // prop: "prefix"
+            // value: ["+"]
+            // (yes it's an array, we join it further down!)
+
+            // We can check that the key exists to avoid having multiple useless, 
+            // unused keys in the config:
+            if(!client.settings.has(message.guild.id, prop)) {
+            return message.reply("This key is not in the configuration.");
+            }
+
+            // Now we can finally change the value. Here we only have strings for values 
+            // so we won't bother trying to make sure it's the right type and such. 
+            client.settings.set(message.guild.id, value.join(" "), prop);
+
+            // We can confirm everything's done to the client.
+            message.channel.send(`Guild configuration item ${prop} has been changed to:\n\`${value.join(" ")}\``);
+            break;
+    }
+}
+
+/** Processes a message and try to upload any images found in it */
+async function processMessageUpload(msg) {
 
     //See if it is a gallery post
     let gallery = await findGalleryFromMessageContent(msg.content);
@@ -114,7 +188,7 @@ discord.on('message', async (msg) => {
         await reaction.remove();
     }
     
-});
+}
 
 /** React to the gall images */
 discord.on('raw', async (packet) => {
